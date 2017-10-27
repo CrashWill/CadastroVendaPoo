990d646bef541d668dacf0e5bf9d7345e1fc65e8
William Cavasotto <pioneirowill@gmail.com>
2017-10-27 19:20:22 -0200

    inclusão do Sumario da Classe Venda

--- Classes/Venda.cs
+++ Classes/Venda.cs
@@ -6,6 +6,13 @@ namespace CadastroVendaPoo.Classes{
         private int id;
         public DateTime datavenda;
         public Venda(){
+            /// <summary>
+            /// Constroi o objeto venda e voce precisa passar os dados como parametro
+            /// </summary>
+            /// <param name="cpf">Passe o cpf do cliente</param>
+            /// <param name="id">Passe a id do produto da venda</param>
+            /// <param name="datavenda">Passe a data da venda do produto</param>
+            /// <returns></returns>
             
         }
         public Venda(string cpf, int id, DateTime datavenda){
